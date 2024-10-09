b326fa4b46fe58fe69fc83c5c6a1f74cea0a202b
AnalizQuezada10 <aquezada8@fordham.edu>
2024-10-08 21:22:57 -0400

    Add files via upload

---
+++ cats.js
@@ -0,0 +1,71 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+const cat = {name:"Pinecone", age:13, type:'Munchkin', cuteness: 9001};
+const catsList = document.createElement('ul');
+catsList.id = 'Cats-List';
+cats.forEach(cat => {
+    const listItems = document.createElement('li');
+    listItems.textContent = Cats;
+    catsList.appendChild(ListItems);
+
+
+});
+//Now append the CatsList to the desired location in the document 
+document.body.appendChild(catsList); 
